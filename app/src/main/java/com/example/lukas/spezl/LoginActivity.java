fd6dcf5e2cd95d2238e830e4d9ffacce513f4021
Lukas Hennig <lukas.hennig@gmx.de>
2017-04-02 21:14:32 +0200

    firebase more stuff

--- app/src/main/java/com/example/lukas/spezl/LoginActivity.java
+++ app/src/main/java/com/example/lukas/spezl/LoginActivity.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.annotation.StringDef;
 import android.support.design.widget.TextInputLayout;
 import android.util.Log;
 import android.view.View;
@@ -87,7 +86,7 @@ public class LoginActivity extends Activity {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (!task.isSuccessful()) {
-                            Toast.makeText(LoginActivity.this, "Anmeldung ist fehlgeschlagen!", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(LoginActivity.this, "E-Mail und Passwort stimmen nicht überein!", Toast.LENGTH_SHORT).show();
                         } else {
                             Intent intent = new Intent(LoginActivity.this, DecisionActivity.class);
                             startActivity(intent);
